9aae43e5dabf6d1781d01b005831f275acc47c18
Dolores Dominguez <oranotherway@gmail.com>
2023-09-09 19:57:09 -0300

    Feature: creación de lista de publicaciones de perros

--- back/proyecto_perros/pagina_perros/urls.py
+++ back/proyecto_perros/pagina_perros/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PublicacionCreate
+from .views import PublicacionCreate, PublicacionList
 
 urlpatterns = [
     path('publicaciones/', PublicacionCreate.as_view(), name = 'publi-create'),
+    path('publicaciones/list/', PublicacionList.as_view(), name = 'publi-list'),
 ]
