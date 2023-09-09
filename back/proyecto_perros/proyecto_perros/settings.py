fb3848ee46accca1f5516e208df141fa0a67bdcd
kralsito <franco_kral@hotmail.com>
2023-09-09 20:05:10 -0300

    Instalacion de Cors

--- back/proyecto_perros/proyecto_perros/settings.py
+++ back/proyecto_perros/proyecto_perros/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'pagina_perros',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -71,7 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'proyecto_perros.wsgi.application'
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
