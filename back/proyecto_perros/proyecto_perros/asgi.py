edcb0b8f1b7a7503f0b291cc1ea2c9092c2841ee
kralsito <franco_kral@hotmail.com>
2023-09-09 15:35:36 -0300

    comienzo de proyecto

---
+++ back/proyecto_perros/proyecto_perros/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for proyecto_perros project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_perros.settings')
+
+application = get_asgi_application()
---
+++ back/proyecto_perros/proyecto_perros/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for proyecto_perros project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_perros.settings')
+
+application = get_wsgi_application()
