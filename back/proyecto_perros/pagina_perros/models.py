dd9a38dd42304cc388ead0e402ba598a95a6be6f
kralsito <franco_kral@hotmail.com>
2023-09-09 18:05:33 -0300

    Feature: Endopoint para dar de alta una publicacion, y modelo de esta ultima

--- back/proyecto_perros/pagina_perros/models.py
+++ back/proyecto_perros/pagina_perros/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Publicacion(models.Model):
+    nombrePerro = models.CharField(max_length=30)
+    fotoPerro = models.ImageField(upload_to="./media/perros", null=True, blank=True)
+    edadPerro = models.CharField(max_length=20)
+    SEXO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    ]
+    sexoPerro = models.CharField(max_length=3, choices=SEXO_CHOICES)
+
+    TAMANO_CHOICES = [
+        ('Pequeño', 'Pequeño'),
+        ('Mediano', 'Mediano'),
+        ('Grande', 'Grande'),
+    ]
+    tamañoPerro = models.CharField(max_length=10, choices=TAMANO_CHOICES)
+
+    #Funcion para retornar algo cuando llamo al objeto
+    def __str__(self):
+        return self.nombrePerro
+    
