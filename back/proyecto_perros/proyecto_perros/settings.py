a27f7c613199ce2979cf2ac14cb8975709d2cceb
kralsito <franco_kral@hotmail.com>
2023-09-09 23:38:29 -0300

    Fix: Error al levantar Django solucionado

--- back/proyecto_perros/proyecto_perros/settings.py
+++ back/proyecto_perros/proyecto_perros/settings.py
@@ -43,8 +43,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
