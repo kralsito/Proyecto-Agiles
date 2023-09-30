938f8df5060cec2df76c9897b689ac998964f1e2
santilibano7 <santilibano27@gmail.com>
2023-09-30 10:32:30 -0300

    Solucion al bug de la componente Publicacion y Footer

--- front/front_perros/src/app/app.component.ts
+++ front/front_perros/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { HomeService } from './home.service';
 
 @Component({
   selector: 'app-root',
@@ -11,26 +10,5 @@ export class AppComponent {
 
   title = 'RouteApp';
 
-  constructor(private router: Router, public homeService: HomeService) {}
-
-  verPublicaciones() {
-    this.homeService.setMostrarPublicacion(true);
-    this.homeService.setMostrarContenido(false);
-    this.homeService.setMostrarInicio(true);
-    this.router.navigate(['/publicacion']);
-  }
-
-  verApp() {
-    this.homeService.setMostrarPublicacion(false);
-    this.homeService.setMostrarContenido(false);
-    this.homeService.setMostrarInicio(false);
-    this.router.navigate(['/']);
-  }
-
-  crearPublicacion() {
-    this.homeService.setMostrarInicio(true);
-    this.homeService.setMostrarPublicacion(false);
-    this.homeService.setMostrarContenido(true);
-    this.router.navigate(['/formpublicacion']);
-  }
+  constructor(private router: Router) {}
 }
