affbbbfce100672437323161df666558568fe594
kralsito <franco_kral@hotmail.com>
2023-09-30 15:59:26 -0300

    feature: url para llamar a la api

--- back/proyecto_perros/pagina_perros/urls.py
+++ back/proyecto_perros/pagina_perros/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PublicacionList, PublicacionCreateView , UsuarioCreateView
+from .views import PublicacionList, PublicacionCreateView , UsuarioCreateView, LoginView
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -8,5 +8,6 @@ urlpatterns = [
     path('publicaciones/alta/', PublicacionCreateView.as_view(), name = 'publi-alta'),
     path('publicaciones/', PublicacionList.as_view(), name = 'publi-list'),
     path('usuario/registrar/', UsuarioCreateView.as_view(), name='registrar-usuario'),
+    path('usuario/login/', LoginView.as_view(), name='login'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
