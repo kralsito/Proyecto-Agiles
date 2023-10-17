df3ab9bef3f6fb9d463e8d5c9b830094b596824d
dolodmg <oranotherway@gmail.com>
2023-10-17 00:44:07 -0300

    Feature: filtros para las publicaciones

--- front/front_perros/src/app/publicacion/publicacion.component.ts
+++ front/front_perros/src/app/publicacion/publicacion.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PublicacionService } from './publicacion.service';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-publicacion',
@@ -10,7 +9,11 @@ import { Router } from '@angular/router';
 })
 export class PublicacionComponent implements OnInit {
   publicaciones: any;
+  publicacionesOriginales: any;
   faHeart = faHeart;
+  filtrosSexo: string[] = [];
+  filtrosTamanio: string[] = [];
+  display = "none";
 
   constructor(private publicacionService: PublicacionService) {}
 
@@ -18,10 +21,61 @@ export class PublicacionComponent implements OnInit {
     this.publicacionService.obtenerPublicaciones().subscribe(
       (data: any) => {
         this.publicaciones = data;
+        this.publicacionesOriginales = [...data];
       },
       (error) => {
         console.error('Error al cargar las publicaciones:', error);
       }
     );
+  } 
+  
+  abrirFiltros() {
+    this.display= "block";
+    const contenidoPrincipal = document.getElementById("contenidoPrincipal");
+    if (contenidoPrincipal) {
+        contenidoPrincipal.style.display = "block";
+    }
+  }
+  onCloseHandled() {
+    this.display = "none";
+  }
+
+  cancelarFiltros() {
+    this.display = "none";
+    return this.publicaciones = [...this.publicacionesOriginales];
+  }
+
+  toggleFiltroSexo(sexo: string) {
+    if (this.filtrosSexo.includes(sexo)) {
+      this.filtrosSexo = this.filtrosSexo.filter((filtro) => filtro !== sexo);
+    } else {
+      this.filtrosSexo.push(sexo);
+    }
+    this.aplicarFiltros();
+  }
+  
+  toggleFiltroTamanio(tamanio: string) {
+    if (this.filtrosTamanio.includes(tamanio)) {
+      this.filtrosTamanio = this.filtrosTamanio.filter((filtro) => filtro !== tamanio);
+    } else {
+      this.filtrosTamanio.push(tamanio);
+    }
+    this.aplicarFiltros();
+  }
+  
+  aplicarFiltros() {
+    if (this.filtrosSexo.length === 0 && this.filtrosTamanio.length === 0) {
+      this.publicaciones = [...this.publicacionesOriginales];
+      return;
+    }
+    const publicacionesFiltradasSexo = this.publicacionesOriginales.filter((publicacion: any) => {
+      return this.filtrosSexo.length === 0 || this.filtrosSexo.includes(publicacion.sexoPerro);
+    });
+
+    const publicacionesFiltradasTamanio = publicacionesFiltradasSexo.filter((publicacion: any) => {
+      return this.filtrosTamanio.length === 0 || this.filtrosTamanio.includes(publicacion.tamanioPerro);
+    });
+
+    this.publicaciones = publicacionesFiltradasTamanio;
   }
 }
