2df1445b9325f9e892f715c217bb1824febddeb1
kralsito <franco_kral@hotmail.com>
2023-10-18 23:32:33 -0300

    feature: endpoint para poder dar de alta un perfil

--- back/proyecto_perros/pagina_perros/views.py
+++ back/proyecto_perros/pagina_perros/views.py
@@ -105,3 +105,11 @@ class MiPerfilView(APIView):
         else:
             return Response({"mensaje": "El usuario no tiene un perfil."}, status=status.HTTP_404_NOT_FOUND)
         
+class PerfilCreateView(APIView):
+    
+    def post(self, request):
+        serializer = PerfilSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
