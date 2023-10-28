8daf09d7292ee54a4a07cd2562ea9a23980cbab4
dolodmg <oranotherway@gmail.com>
2023-10-28 15:00:07 -0300

    Feature: guard para usuarios con sesión iniciada

--- front/front_perros/src/app/app-routing.module.ts
+++ front/front_perros/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './login/login.component';
 import { FormusuarioComponent } from './formusuario/formusuario.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { authGuard } from './guards/auth.guard';
+import { authGuardForLogin } from './guards/sesion-guard.guard';
 import { NavBarNoLogueadoComponent } from './nav-bar-no-logueado/nav-bar-no-logueado.component';
 
 const routes: Routes = [
@@ -18,8 +19,8 @@ const routes: Routes = [
   { path: 'nav-bar', component: NavBarComponent, canActivate: [authGuard] },
   { path: 'menu-principal', component: MenuPrincipalComponent, canActivate: [authGuard]},
   { path: 'titulo-yeslogan', component: TituloYEsloganComponent, canActivate: [authGuard]},
-  { path: 'login', component: LoginComponent},
-  { path: 'registro', component: FormusuarioComponent},
+  { path: 'login', component: LoginComponent, canActivate: [authGuardForLogin]},
+  { path: 'registro', component: FormusuarioComponent, canActivate: [authGuardForLogin]},
   { path: 'perfil', component: PerfilComponent},
   { path: 'nav-bar-no-logueado', component: NavBarNoLogueadoComponent}
 ];
