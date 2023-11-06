3f08af4ec811af233005923469f4bd1aa3b3c2cf
kralsito <franco_kral@hotmail.com>
2023-11-06 17:24:21 -0300

    feature: el boton guardar, agrega a favoritos una publicacion

--- front/front_perros/src/app/publicacion/publicacion.service.ts
+++ front/front_perros/src/app/publicacion/publicacion.service.ts
@@ -11,4 +11,16 @@ export class PublicacionService {
   obtenerPublicaciones(): Observable<any> {
     return this.http.get('http://localhost:8000/api/publicaciones/');
   }
+
+  agregarPublicacionAFavoritos(publicacionId: number, usuarioId: number): Observable<any> {
+    // Crear un objeto con los datos que deseas enviar en la solicitud POST, incluyendo el usuarioId
+    const data = {
+      usuarioId: usuarioId,
+      // Otros datos si es necesario
+    };
+  
+    // Realizar una solicitud POST para agregar la publicación a favoritos
+    return this.http.post(`http://localhost:8000/api/publicaciones/${publicacionId}/favorito/`, data);
+  }
+  
 }
